959246f8c386e652415b6935d326a4f9d8a03394
Andre L Cabrera <andre@cabrera.pw>
2019-12-11 00:46:50 -0300

    Add slp middleware

--- app/app.ts
+++ app/app.ts
@@ -1,9 +1,16 @@
 import express = require('express');
+import slpMiddleware from './slpMiddleware';
 import { postageRate } from './config/postage';
 
 const app: express.Application = express();
 
+app.use(slpMiddleware);
+
 app.get('/postage', function(req, res) {
+    res.send(postageRate);
+});
+
+app.post('/postage', function(req, res) {
     res.send(postageRate);
 });
 
---
+++ app/slpMiddleware.js
@@ -0,0 +1,15 @@
+const slpMiddleware = (req, res, next) => {
+    if (!req.is('application/simpleledger-payment')) return next();
+    let data = [];
+    req.on('data', chunk => {
+        data.push(chunk);
+    });
+    req.on('end', () => {
+        if (data.length <= 0) return next();
+        data = Buffer.concat(data);
+        req.raw = data;
+        next();
+    });
+};
+
+export default slpMiddleware;
